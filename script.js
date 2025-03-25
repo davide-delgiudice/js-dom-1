feef0c3a8c6151a0f28ea38db889e3a6625377ed
davide-delgiudice <dav.del.giudice@gmail.com>
2025-03-25 16:47:13 +0100

    [UPD] add EventListener to button

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // recupero l'elemento immagine e pulsante nella pagina
-
 const imageoff = document.querySelector('img');
-const button = document.getElementById('turn')
+const button = document.getElementById('turn');
+
+// creo una funzione per il bottone
+button.addEventListener('click', function() {
+    console.log('Cliccato!');
+})
