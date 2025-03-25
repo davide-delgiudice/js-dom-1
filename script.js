974fb91530a8df036d87ac1cd2e6d752688b487d
davide-delgiudice <dav.del.giudice@gmail.com>
2025-03-25 17:57:45 +0100

    [UPD] changed InnerText in if condition - Bonus Terminato

--- script.js
+++ script.js
@@ -6,13 +6,15 @@ const button = document.getElementById('turn');
 // creo una funzione per il bottone
 button.addEventListener('click', function() {
     console.log('Acceso!');
-    
+
     if(lampon.classList.contains('hidden')){
         lampon.classList.remove('hidden');
         lampoff.classList.add('hidden');
+        button.innerText = 'Spegni';
     }
     else{
         lampoff.classList.remove('hidden');
         lampon.classList.add('hidden');
+        button.innerText = 'Accendi';
     }
 })
