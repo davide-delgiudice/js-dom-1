dc5c169eff7e21f90ac61b25a58a6cee1612d632
davide-delgiudice <dav.del.giudice@gmail.com>
2025-03-25 17:23:50 +0100

    [UPD] add classes spento and acceso

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // recupero l'elemento immagine e pulsante nella pagina
-const image = document.querySelector('img');
+const lampoff = document.querySelector('.spento');
+const lampon = document.querySelector('.acceso')
 const button = document.getElementById('turn');
 
 // creo una funzione per il bottone
 button.addEventListener('click', function() {
     console.log('Acceso!');
+
 })
